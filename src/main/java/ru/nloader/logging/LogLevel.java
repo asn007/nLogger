5eb1ed8e7103b7d04689485f770397f8c0b3fe31
asn007 <andrey.sin98@gmail.com>
2015-06-11 11:49:35 +0300

    I always forget about license

--- src/main/java/ru/nloader/logging/LogLevel.java
+++ src/main/java/ru/nloader/logging/LogLevel.java
@@ -1,20 +1,20 @@
 /*
- * Copyright (C) 2014 asn007 aka Andrey Sinitsyn <andrey.sin98@gmail.com>
+ * Copyright (C) 2015 asn007 aka Andrey Sinitsyn <andrey.sin98@gmail.com>
  *
- * This file (LogLevel.java) is part of nLogger.
+ *  This file (LogLevel.java) is part of nLogger.
  *
- *     nLogger is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ *      nLogger is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
  *
- *     nLogger is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ *      nLogger is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *      You should have received a copy of the GNU General Public License
+ *      along with nLogger.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package ru.nloader.logging;
